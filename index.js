4a39cb2f6685381c1f44782902e2dc01d30c3bf0
JD <jdmejia712@gmail.com>
2021-09-10 13:44:00 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return x - y;
+}
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
+
+function increment(n) {
+    return n+1;
+}
+
+function decrement(n) {
+    return n-1;
+}
+
+function increment(n) {
+    return n+1;
+}
+
+function makeInt(n) {
+	return parseInt.n;
+}
+
+function makeInt(n) {
+	return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n); 
+}
+
+/* 
+  makeInt(n)
+  11) parses n as an integer and returns the parsed integer
+  12) assumes base 10
+  13) returns NaN as appropriate
+
+preserveDecimal(n)
+  14) preserves n's decimals (it parses n as a floating point number) and returns the parsed number
+  15) returns NaN as appropriate
+*/
